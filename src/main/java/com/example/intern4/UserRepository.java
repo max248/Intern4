1f7d4d3c72f557eb6e648dc0c84657e381700ebb
Abbos <yoldoshev-1994@mail.ru>
2022-08-04 00:33:19 +0500

    bug fix

--- src/main/java/com/example/intern4/UserRepository.java
+++ src/main/java/com/example/intern4/UserRepository.java
@@ -6,15 +6,10 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u where u.email = ?1")
     User findByEmail(String email);
-
-//    @Query("SELECT u FROM User u where u.id = ?1")
-//    User findById(Long id);
 
     @Modifying
     @Query("update User u set u.lastLoginDate = ?2 where u.id = ?1")
